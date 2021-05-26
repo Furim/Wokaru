5f384f4649b58436a7f463ef25429cbaf9fa815d
Furim <furimmm@furimmm.home>
2021-05-26 17:12:48 +0200

    Added script to wokaru, archiving soon

--- script.sh
+++ script.sh
@@ -2,6 +2,25 @@
 
 
 
+echo ///---																																																																					---///
+echo //Password manager will ask you about your password and save it to the file and then encrypt, soon will be added the archiving the files/passwords //
+echo ///																																																																						---///
+
 
 g++ main.cpp
+
 ./a.out
+
+
+
+echo Your password
+
+cat my_file.txt
+
+gpg -c --no-symkey-cache --cipher-algo AES256 my_file.txt
+
+
+rm -rf myfile.txt
+echo decrypt it if u want get the password ofc :p 
+
+echo File sucessfully encrypted
